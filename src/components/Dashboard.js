a650211c7b1b31e017ec76976520cab54ef60c5d
mattfeng <matthewfznn@gmail.com>
2024-05-20 00:40:07 -0400

    add overview block

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -3,6 +3,7 @@ import Card from "./Card";
 import { mockCompanyDetails } from "../constants/mock";
 import Header from "./Header";
 import Details from "./Details";
+import Overview from "./Overview";
 
 const Dashboard = () => {
   return (
@@ -14,7 +15,13 @@ const Dashboard = () => {
         <Card>Chart</Card>
       </div>
       <div>
-        <Card>Overview</Card>
+        <Overview
+          symbol={mockCompanyDetails.ticker}
+          price={300}
+          change={30}
+          changePercent={10.0}
+          currency={"USD"}
+        />
       </div>
       <div className="row-span-2 xl:row-span-3">
         <Details details={mockCompanyDetails} />
