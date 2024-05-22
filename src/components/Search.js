f8f870d9420844dfe9618e8b7911e4f7191ad86a
mattfeng <matthewfznn@gmail.com>
2024-05-21 21:05:44 -0400

    add dark mode function

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,12 +1,15 @@
-import React, { useState } from "react";
+import React, { useContext, useState } from "react";
 import { mockSearchResults } from "../constants/mock";
 import { XMarkIcon } from "@heroicons/react/16/solid";
 import { MagnifyingGlassIcon } from "@heroicons/react/16/solid";
 import SearchResults from "./SearchResults";
+import ThemeContext from "../context/ThemeContext";
 
 const Search = () => {
   const [input, setInput] = useState("");
   const [bestMatches, setBestMatches] = useState(mockSearchResults.result);
+
+  const { darkMode } = useContext(ThemeContext);
 
   const clear = () => {
     setInput("");
@@ -18,11 +21,17 @@ const Search = () => {
   };
 
   return (
-    <div className="flex items-center my-4 border-2 rounded-md relative z-50 w-96 bg-white border-neutral-200">
+    <div
+      className={`flex items-center my-4 border-2 rounded-md relative z-50 w-96 ${
+        darkMode ? "bg-gray-900 border-gray-800" : "bg-white border-neutral-200"
+      }`}
+    >
       <input
         type="text"
         value={input}
-        className="w-full px-4 y-2 focus:outline-none rounded-md"
+        className={`w-full px-4 y-2 focus:outline-none rounded-md ${
+          darkMode ? "bg-gray-900" : null
+        }`}
         placeholder="Search stock..."
         onChange={(event) => {
           setInput(event.target.value);
